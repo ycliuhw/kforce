82573d44164b5228045bd20022067773d8079f8d
Kelvin Liu <ycliuhw@gmail.com>
2018-02-16 23:51:09 +1100

    Kelvin: added tests

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import os
 import sys
-import pytest
 
 # Prepend src directory to python path
 this_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(1, os.path.join(this_dir, 'lib'))
+
+# os.environ['AWS_PROFILE'] = 'this is not a real profile'
--- tests/unit/test_aws_facts.py
+++ tests/unit/test_aws_facts.py
@@ -1,3 +1,4 @@
+import os
 from unittest import TestCase
 
 from lib.aws_facts import get_vpc_facts
@@ -6,5 +7,9 @@ from moto import mock_ec2
 
 @mock_ec2
 class TestAwsFacts(TestCase):
+
+    def setUp(self):
+        ...
+
     def test_check_rt_internet_facing(self):
-        pass
+        ...
