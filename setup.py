3701029b783995d1bed1db0e8fc08fa82a5cf523
balan <umabalan.nagarajan@domain.com.au>
2018-03-07 10:57:04 +1100

    uncommented url

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         author="Yang Kelvin Liu",
         author_email="ycliuhw@gmail.com",
         license="Apache License 2.0",
-        #url="https://github.com/ycliuhw/kforce",
+        url="https://github.com/ycliuhw/kforce",
         description="KOPS template automation",
         packages=["kforce"],
         scripts=scripts,
